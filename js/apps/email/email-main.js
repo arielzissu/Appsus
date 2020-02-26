4fa79bf5a56a892e7f09d6a24a08e49570b533a8
roi.ilan <roiilan23@gmail.com>
2020-02-26 14:25:05 +0200

    sdsd

---
+++ js/apps/email/email-main.js
@@ -0,0 +1,21 @@
+
+export default {
+    template: `
+    <section class="email-container">
+        <h1>heyyy</h1>
+    </section>
+    `,
+    data(){
+        return {
+     
+        }
+    },
+    
+    created(){
+       
+    },
+    methods: {
+       
+        
+    }
+}
--- js/pages/mister-email.js
+++
@@ -1,7 +0,0 @@
-export default {
-    template: `
-    <section class="email-container">
-        <h1>heyyy</h1>
-    </section>
-    `,
-}
