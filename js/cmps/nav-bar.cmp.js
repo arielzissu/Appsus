514a2803f8f657da39e8620e42690c379638bd45
Your Nameariel <arielzissu98@gmail.com>
2020-02-26 11:30:49 +0200

    my1

--- js/cmps/nav-bar.cmp.js
+++ js/cmps/nav-bar.cmp.js
@@ -1,29 +1,18 @@
-// export default {
-//     template: `
-//         <div class="navbar">
-//             <button class="nav-btn first" @click="goBack">GO BACK</button>
-//             <router-link class="nav-btn underLine-none" to="/" exact>
-//                 Home
-//             </router-link>
-//             <router-link class="nav-btn underLine-none" to="/email" exact>
-//             Mister Email
-//             </router-link>
-//         </div>
-//     `,
-//     methods: {
-//         goBack() {
-//             this.$router.go(-1)
-//         }
-//     }
-// }
-
-
 export default {
     template: `
         <div class="navbar">
-            <router-link class="nav-btn underLine-none" to="/">
+            <button class="nav-btn first" @click="goBack">GO BACK</button>
+            <router-link class="nav-btn underLine-none" to="/" exact>
                 Home
-            </router-link>           
+            </router-link>
+            <router-link class="nav-btn underLine-none" to="/email" exact>
+            Mister Email
+            </router-link>
         </div>
-    `
+    `,
+    methods: {
+        goBack() {
+            this.$router.go(-1)
+        }
+    }
 }
