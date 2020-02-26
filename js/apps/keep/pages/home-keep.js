f3393e330dfd9dee1e827b70a652bbb76656af8c
Your Nameariel <arielzissu98@gmail.com>
2020-02-26 14:25:44 +0200

    asasd

--- js/apps/keep/pages/home-keep.js
+++ js/apps/keep/pages/home-keep.js
@@ -2,6 +2,12 @@ export default {
     template: `
     <section class="home-keep-container">
         <h1>keep App!</h1>
+        <div>
+            <input type="text" placeholder="Text here...">
+            <button>List</button>
+            <button>Text</button>
+            <button>Img</button>
+        </div>
 
     </section>
     `,
