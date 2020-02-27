d2f0eedb4db5bd7a9f83235d82cb6d15a3fc4233
Your Nameariel <arielzissu98@gmail.com>
2020-02-27 14:09:23 +0200

    jhkh

--- js/apps/keep/cmps/card-keep.cmp.js
+++ js/apps/keep/cmps/card-keep.cmp.js
@@ -2,15 +2,15 @@ export default {
     name: 'note-prev',
     template: `
     <section >
-        <div v-if="note.type===0" class="card-txt">
+        <div v-if="note.type==='txt'" class="card-txt">
             <p>{{note.info.txt}}</p>
         </div>
 
-        <div v-if="note.type===1" class="card-txt">
+        <div v-if="note.type==='list'" class="card-txt">
             <h2>note.info.title</h2>
             <img src="note.info.url">
         </div>
-        <div v-if="note.type===2" class="card-img">
+        <div v-if="note.type==='img'" class="card-img">
             <div v-for="todo in note.info.todos">
 
             </div>
