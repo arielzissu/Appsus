cc7cc0d0620ed6cd34c5dcbc8c5ea2cf5c2f891d
Your Nameariel <arielzissu98@gmail.com>
2020-02-27 13:56:59 +0200

    ffrrre

--- js/apps/keep/cmps/list-cards.cmp.js
+++ js/apps/keep/cmps/list-cards.cmp.js
@@ -2,13 +2,13 @@ import cardKeep from './card-keep.cmp.js'
 
 export default {
     template: `
-    <section>
-        <div>
-            <card-keep :numType="numType"></card-keep>
+    <section >
+        <div v-for="note in notes" :key="note.id">
+            <card-keep :note="note"></card-keep>
         </div>
     </section>
     `,
-    props: ['numType', 'text'],
+    props: ['notes'],
 
     components: {
         'card-keep': cardKeep
